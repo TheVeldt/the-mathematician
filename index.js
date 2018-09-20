36e25d6217377829ec9d9a5b4033577e67d7d902
João Pedro Andrade Marques <joao.andrade_marques@siemens.com>
2018-09-20 17:09:30 +0100

    return the full event

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ module.exports.getLastEvent=()=>{
     }
     return null
 }
+module.exports.getFullEventAsString=()=>{
+    fullEvents="";
+    if(events.length==0){
+        return "There are no events to show"
+    }
+    fullEvents=fullEvents+"1st - "+ events[0] +"\n";
+    if(events.length>1){
+        fullEvents=fullEvents+"2nd - "+ events[1] +"\n";
+    }
+    if(events.length>2){
+        fullEvents=fullEvents+"3rd - "+ events[2] +"\n";
+    }
+    for(i=3;i<=event.length;i++){
+            fullEvents=fullEvents+i.toString()+"th - "+ events[i]+"\n";
+    }
+    return fullEvents;
+}
